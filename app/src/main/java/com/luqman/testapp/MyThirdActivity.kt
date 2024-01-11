8afe5a082dec1518b857fd9e241ff2f666414212
nasib <nasibzada12@gmail.com>
2024-01-11 12:55:45 +0500

    class no.6

--- app/src/main/java/com/luqman/testapp/MyThirdActivity.kt
+++ app/src/main/java/com/luqman/testapp/MyThirdActivity.kt
@@ -2,23 +2,24 @@ package com.luqman.testapp
 
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MyThirdActivity : AppCompatActivity() {
 
-    var prop1 = 1
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_my_third)
 
-        val myButton1:TextView  = findViewById(R.id.my_button)
+        val myBtn: TextView = findViewById(R.id.my_button)
 
-        myButton1.setOnClickListener {
+        myBtn.setOnClickListener {
 
+            val myToast = Toast.makeText(this, "You clicked a btn", Toast.LENGTH_SHORT)
+            myToast.show()
 
         }
+
 
 
 
